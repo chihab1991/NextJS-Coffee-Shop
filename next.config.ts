323b93e4c95e9016dda3784556a4f60dd954c447
chihab1991 <chihab.chergui.1991@gmail.com>
2024-12-02 22:17:14 +0100

    fixes to deploy to vercel

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:"export",
-  basePath:"/nextjs-coffee-shop"
+
 };
 
 export default nextConfig;
